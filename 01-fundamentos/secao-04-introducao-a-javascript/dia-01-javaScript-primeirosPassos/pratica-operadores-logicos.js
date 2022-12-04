4a19dfcc529c693d461dfa5a2f9656e1ccffb27e
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2022-12-04 18:23:04 -0300

    Finalizado pratica-operadores-logicos

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javaScript-primeirosPassos/pratica-operadores-logicos.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javaScript-primeirosPassos/pratica-operadores-logicos.js
@@ -26,19 +26,34 @@
 
 // ------------------------------------------------------------------------------------
 
-// Operadores lógicos (Operador OR ||)
-const bebidaPrincipal =  'cafezinho';
-const bebidaAlternativa = 'suco de laranja';
+// // Operadores lógicos (Operador OR ||)
+// const bebidaPrincipal =  'cafezinho';
+// const bebidaAlternativa = 'suco de laranja';
 
-if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
-  console.log("Obrigado por me atender :D")
-} else {
-  console.log("Ei, eu não pedi isso!");
-}
+// if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+//   console.log("Obrigado por me atender :D")
+// } else {
+//   console.log("Ei, eu não pedi isso!");
+// }
 
-console.log(true || true); // true
-console.log(true || false); // true
-console.log(false || true); // true
-console.log(false || false); // false
+// console.log(true || true); // true
+// console.log(true || false); // true
+// console.log(false || true); // true
+// console.log(false || false); // false
 
 //--------------------------------------------------------------------------------------
+
+// // Operadores lógicos (Operador NOT !)
+// console.log((2 + 2) === 4);
+// console.log(!(2 + 2) === 4);
+
+// const squirtle = "melhor pokemon inicial";
+// console.log(!squirtle); // false
+
+// console.log(!42); // false
+// console.log(!0); // true
+// // O número 0 tem o valor "falsy" no javascript. Logo, seu oposto é true.
+
+// console.log(!null); // true
+
+// console.log(!undefined); // true
