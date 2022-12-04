2ecd1cc60001e03b70b9a6515134f109365ad199
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2022-12-04 18:12:50 -0300

    Para fixar 2 (Operadores Lógicos) resolvido!

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javaScript-primeirosPassos/operadores-logicos.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javaScript-primeirosPassos/operadores-logicos.js
@@ -17,5 +17,12 @@
 // console.log(message);
 //--------------------------------------------------------
 
-//PARA FIXAR 2
-
+// //PARA FIXAR 2
+// // Operador OR
+// let weekDay = 'quarta-feira';
+// if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+//     console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+// } else {
+//     console.log('FINALMENTE, descanso merecido UwU');
+// }
+//--------------------------------------------------------
