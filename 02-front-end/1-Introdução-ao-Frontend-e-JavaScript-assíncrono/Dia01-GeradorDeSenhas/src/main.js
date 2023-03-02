eb888ac21822eba0d61d2ceb43f018e6b687248e
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2023-03-02 16:47:11 -0300

    Implementando funcionalidades

--- 02-front-end/1-Introdução-ao-Frontend-e-JavaScript-assíncrono/Dia01-GeradorDeSenhas/src/main.js
+++ 02-front-end/1-Introdução-ao-Frontend-e-JavaScript-assíncrono/Dia01-GeradorDeSenhas/src/main.js
@@ -1,3 +1,11 @@
 import { nanoid } from 'nanoid';
 
+const buttonPassword = document.querySelector('button');
+const h2 = document.querySelector('h2');
+
+buttonPassword.addEventListener('click', () => {
+    h2.innerHTML = nanoid();
+});
+
 console.log(nanoid());
+console.log(h2);
