67fa33bd42bdc5a314958666d41250fde13fbb82
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2023-04-03 19:11:10 -0300

    Extensão do exercicio 2 add

--- 02-front-end/3- Componentes com Estado, Eventos e Formulários com React/Dia 1 - Componentes com Estado e Eventos/exercicio-de-fixacao/exercicio-2-constructor-e-bind/src/App.js
+++ 02-front-end/3- Componentes com Estado, Eventos e Formulários com React/Dia 1 - Componentes com Estado e Eventos/exercicio-de-fixacao/exercicio-2-constructor-e-bind/src/App.js
@@ -4,28 +4,41 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      botaoUm : 0,
+      botaoDois : 0,
+      botaoTres: 0
+    }
+
     this.handleButtonOne = this.handleButtonOne.bind(this);
     this.handleButtonTwo = this.handleButtonTwo.bind(this);
     this.handleButtonThree = this.handleButtonThree.bind(this);
   }
 
   handleButtonOne() {
-    console.log('"this" do botão 1 :', this);
+    this.setState(({botaoUm}) => ({
+      botaoUm: botaoUm + 1
+    }))
   }
 
   handleButtonTwo() {
-    console.log('"this" do botão 2 :', this);
+    this.setState((prevState) => ({
+      botaoDois: prevState.botaoDois + 1
+    }))
   }
 
   handleButtonThree() {
-    console.log('"this" do botão 3 :', this);
+    this.setState(({botaoTres}) => ({
+      botaoTres: botaoTres + 1
+    }))
   }
   render() {
+    const { botaoDois, botaoTres } = this.state;
     return (
       <div>
-        <button onClick={ this.handleButtonOne }>Botão 1</button>
-        <button onClick={ this.handleButtonTwo }>Botão 2</button>
-        <button onClick={ this.handleButtonThree }>Botão 3</button>
+        <button onClick={ this.handleButtonOne }>{this.state.botaoUm}</button>
+        <button onClick={ this.handleButtonTwo }>{botaoDois}</button>
+        <button onClick={ this.handleButtonThree }>{`Cliques no Botão 3: ${botaoTres}`}</button>
       </div>
     );
   }
