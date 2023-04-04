fb4defb5b50ab43f3e69349103deb0e71c334eda
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2023-04-04 02:02:16 -0300

    Exercicio concluido

--- 02-front-end/3- Componentes com Estado, Eventos e Formulários com React/Dia 1 - Componentes com Estado e Eventos/exercicio-de-fixacao/exercicio-2-constructor-e-bind/src/App.js
+++ 02-front-end/3- Componentes com Estado, Eventos e Formulários com React/Dia 1 - Componentes com Estado e Eventos/exercicio-de-fixacao/exercicio-2-constructor-e-bind/src/App.js
@@ -16,29 +16,57 @@ class App extends React.Component {
   }
 
   handleButtonOne() {
-    this.setState(({botaoUm}) => ({
-      botaoUm: botaoUm + 1
-    }))
+    this.setState((prevState) => ({
+      botaoUm: prevState.botaoUm + 1,
+    }), () => {
+      console.log(`Botão 1 ${this.getButtonColor(this.state.botaoUm)}`);
+    });
   }
 
   handleButtonTwo() {
     this.setState((prevState) => ({
-      botaoDois: prevState.botaoDois + 1
-    }))
+      botaoDois: prevState.botaoDois + 1,
+    }), () => {
+      console.log(`Botão 2 ${this.getButtonColor(this.state.botaoDois)}`);
+    });
   }
 
   handleButtonThree() {
-    this.setState(({botaoTres}) => ({
-      botaoTres: botaoTres + 1
-    }))
+    this.setState((prevState) => ({
+      botaoTres: prevState.botaoTres + 1,
+    }), () => {
+      console.log(`Botão 3 ${this.getButtonColor(this.state.botaoTres)}`);
+    });
   }
+
+  getButtonColor(num) {
+    return num % 2 === 0 ? 'green' : 'white';
+  }
+
   render() {
-    const { botaoDois, botaoTres } = this.state;
+    const { botaoUm, botaoDois, botaoTres } = this.state;
     return (
       <div>
-        <button onClick={ this.handleButtonOne }>{this.state.botaoUm}</button>
-        <button onClick={ this.handleButtonTwo }>{botaoDois}</button>
-        <button onClick={ this.handleButtonThree }>{`Cliques no Botão 3: ${botaoTres}`}</button>
+        <button
+        onClick={ () =>this.handleButtonOne() }
+        style={ {backgroundColor: this.getButtonColor(botaoUm)} }
+        >
+          {`Cliques botão 1: ${botaoUm}`}
+        </button>
+
+        <button
+        style={ {backgroundColor: this.getButtonColor(botaoDois)} }
+        onClick={ () => this.handleButtonTwo() }
+        >
+          {`Cliques botão 2: ${botaoDois}`}
+        </button>
+
+        <button
+        style={ {backgroundColor: this.getButtonColor(botaoTres)} }
+        onClick={ () =>this.handleButtonThree() }
+        >
+          {`Cliques no Botão 3: ${botaoTres}`}
+        </button>
       </div>
     );
   }
