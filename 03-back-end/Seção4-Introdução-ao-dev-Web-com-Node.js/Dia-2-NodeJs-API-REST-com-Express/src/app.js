75c61931a35fc467ce9fc44e6170970c5bbdf48a
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2023-09-18 23:48:46 -0300

    SALVEI EXERCICIOS DO DIA 4.2

--- 03-back-end/Seção4-Introdução-ao-dev-Web-com-Node.js/Dia-2-NodeJs-API-REST-com-Express/src/app.js
+++ 03-back-end/Seção4-Introdução-ao-dev-Web-com-Node.js/Dia-2-NodeJs-API-REST-com-Express/src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
 const app = express();
-const port = 3001;
+
+//LINHA MT IMPORTANTE
+app.use(express.json());
+
+const port = 3004;
 const users = [
   {
     id: 1,
@@ -20,14 +24,59 @@ const users = [
   }
 ];
 
+//CRIANDO A ROTA RAIZ
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'okay'
   })
 });
 
+// CRIANDO ROTA USERS E DEVOLVENDO OBJETO
 app.get('/users', (req,res) => {
   return res.status(200).json(users)
 });
 
-app.listen(port, () => console.log('Server rodando na porta 3001'));
+// PARA DEIXAR DINÂMICO SE USA : (DOIS PONTOS)
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const user = users.find((user) => user.id === id)
+  if (!user) {
+    res.status(404).json({
+      message: `Usuário com o ID ${id} não encontrado!`
+    })
+  }
+  return res.status(200).json({
+    user,
+  })
+});
+
+//FAZENDO CADASTROS
+app.post('/users', (req, res) => {
+  const { name } = req.body;
+  const nextId = users.length+1;
+  users.push({ id: nextId, name, active: true })
+  return res.status(201).json({
+    message: "Pessoa Usuária criada com sucesso!"
+  });
+})
+
+//CRIANDO ROTA PARA DELETAR
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  //procuro o id informado
+  const idx = users.findIndex((user) => user.id === +id);
+  //se nao tiver retorna o 404
+  if (idx === -1) {
+    return res.status(404).json({
+      message: `Usuário com o ID ${id} não existe`
+    })
+  }
+  //quando deletar
+  users.splice(idx, 1);
+  return res.status(204).json({
+    message: 'Deletado com sucesso!'
+  })
+});
+
+// O EXPRESS FICA OUVINDO EM UMA PORTA A NOSSA ESCOLHA PRA RODAR O BACK-END
+app.listen(port, () => console.log('Server rodando na porta 3004'));
