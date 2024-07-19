4b72b68f23a5374e81d18fc117be350c498617cb
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-07-19 00:29:47 -0300

    Resolve exercicio 6 - fix

--- M1 - Fundamentos/S3 - Introdução à JavaScript/3.2 - Array e Estruturas de Repetição/exercicio6.js
+++ M1 - Fundamentos/S3 - Introdução à JavaScript/3.2 - Array e Estruturas de Repetição/exercicio6.js
@@ -1,4 +1,4 @@
-let inverter = "Lorena";
+let inverter = "Alisson";
 // Usando o método split para transformar a string em um array, o método reverse para inverter a ordem dos elementos do array e o join para transformar o array em uma string novamente
 let invertida = inverter.split("").reverse().join("");
 console.log(invertida.toLowerCase());
