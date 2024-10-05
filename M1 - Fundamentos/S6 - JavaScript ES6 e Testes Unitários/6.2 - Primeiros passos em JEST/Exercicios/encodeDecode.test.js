72175f5df815ca53369f9b0072d93fe56fe7f4fd
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-10-05 15:59:34 -0300

    FUND 6.2 - Cria teste 1/5 para função encodeDecode.js

---
+++ M1 - Fundamentos/S6 - JavaScript ES6 e Testes Unitários/6.2 - Primeiros passos em JEST/Exercicios/encodeDecode.test.js
@@ -0,0 +1,23 @@
+const { encode, decode } = require("./encodeDecode");
+
+describe("Testa a função encode e decode", () => {
+  it("Teste se encode e decode são funções.", () => {
+    expect(typeof encode && typeof decode).toBe("function");
+  });
+
+  // it("", () => {
+  //   expect().toBe("");
+  // });
+
+  // it("Teste se encode e decode são funções.", () => {
+  //   expect().toBe("");
+  // });
+
+  // it("Teste se encode e decode são funções.", () => {
+  //   expect().toBe("");
+  // });
+
+  // it("Teste se encode e decode são funções.", () => {
+  //   expect().toBe("");
+  // });
+});
