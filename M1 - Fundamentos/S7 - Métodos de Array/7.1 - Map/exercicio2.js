895e005698f5b3a7a6d22334d702c8d62ea4ffd1
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-10-16 22:04:26 -0300

    Refatoração de código no exercicio de Map

--- M1 - Fundamentos/S7 - Métodos de Array/7.1 - Map/exercicio2.js
+++ M1 - Fundamentos/S7 - Métodos de Array/7.1 - Map/exercicio2.js
@@ -1,13 +1,11 @@
 const numbers = [4, 9, -1, 16, -5, 25];
 
-const raiz = (array) => {
-  return array.map((num) => {
+const raiz = (array) => array.map((num) => {
     if (num < 0) {
       num === NaN;
     }
 
     return Math.sqrt(num);
   });
-};
 
 console.log(raiz(numbers));
