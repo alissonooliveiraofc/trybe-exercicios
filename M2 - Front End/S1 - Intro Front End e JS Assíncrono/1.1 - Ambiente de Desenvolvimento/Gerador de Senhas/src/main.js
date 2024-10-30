1193e8b0765a79c0fe21c18da662c64a401dc857
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-10-30 16:04:13 -0300

    FRONT 1.1 - Ajusta aplicação de ID's aleatórios e seu css

--- M2 - Front End/S1 - Intro Front End e JS Assíncrono/1.1 - Ambiente de Desenvolvimento/Gerador de Senhas/src/main.js
+++ M2 - Front End/S1 - Intro Front End e JS Assíncrono/1.1 - Ambiente de Desenvolvimento/Gerador de Senhas/src/main.js
@@ -1,12 +1,20 @@
 import { nanoid } from 'nanoid';
 import './style.css';
+import copy from 'clipboard-copy';
 
 const buttonPassword = document.querySelector('button');
 const h2 = document.querySelector('h2');
 
 buttonPassword.addEventListener('click', () => {
   h2.innerHTML = nanoid();
+  h2.style.color = '#e8a4f0';
+  h2.style.cursor = 'pointer';
 });
 
-console.log(nanoid());
-console.log(h2);
+h2.addEventListener('click', (event) => {
+  copy(event.target.innerText);
+  event.target.innerText = 'Copiado!';
+  event.target.style = 'color: green';
+});
+
+h2.style.cursor = 'pointer';
