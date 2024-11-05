d85e5207230ffeee5f6c61e9cff2e49fc1572ed9
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-11-05 17:31:47 -0300

    FRONT 1.2 - Finaliza exercicios API heroes

--- M2 - Front End/S1 - Intro Front End e JS Assíncrono/1.2 - Promises & Fetch(APIS)/exercicio 2 - API super hero/src/main.js
+++ M2 - Front End/S1 - Intro Front End e JS Assíncrono/1.2 - Promises & Fetch(APIS)/exercicio 2 - API super hero/src/main.js
@@ -1,1 +1,26 @@
-console.log("olá");
+import Swal from "sweetalert2";
+import "sweetalert2/src/sweetalert2.scss";
+
+const img = document.querySelector("img");
+const heroName = document.querySelector("#hero-name");
+const button = document.querySelector("button");
+
+const randomNumber = () => Math.ceil(Math.random() * 731);
+
+button.addEventListener("click", () => {
+  fetch(
+    `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${randomNumber()}.json`
+  )
+    .then((response) => response.json())
+    .then(({ images, name }) => {
+      img.src = images.md;
+      heroName.innerText = name;
+    })
+    .catch((error) => {
+      Swal.fire({
+        icon: "error",
+        title: "Oops... Temos um problema!",
+        text: error.message,
+      });
+    });
+});
