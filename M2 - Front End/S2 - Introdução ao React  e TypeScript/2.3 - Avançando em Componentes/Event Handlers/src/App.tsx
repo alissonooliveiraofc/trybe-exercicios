2a83720fc2c1ac269be943ee7d16ea1866531651
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-12-03 16:20:01 -0300

    FRONT 2.3 - Adiciona Generics e handleChange ao app.tsx

--- M2 - Front End/S2 - Introdução ao React  e TypeScript/2.3 - Avançando em Componentes/Event Handlers/src/App.tsx
+++ M2 - Front End/S2 - Introdução ao React  e TypeScript/2.3 - Avançando em Componentes/Event Handlers/src/App.tsx
@@ -7,10 +7,18 @@ function App() {
     alert(text);
   }
 
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.value);
+  }
+    
+
   return (
     <>
       <Button onClick={() => handleClick('Botão 1 na tela!')}>BOTÃO 1</Button>
       <Button onClick={() => handleClick('Botão 2 na tela!')}>BOTÃO 2</Button>
+
+      <p>Veja o console log enquanto digita...</p>
+      <input name='input' type="text" onChange={(event) => handleChange(event)}/>
     </>
   )
 }
