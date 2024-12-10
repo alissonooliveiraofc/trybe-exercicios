1bd7aec9c735544e3092fce0c3ac337425ae7a7c
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-12-10 19:45:04 -0300

    2.5 - Termina aplicação com algum css sobre eventos e mudança na renderização da aplicação

--- M2 - Front End/S2 - Introdução ao React  e TypeScript/2.5 - Estados e Eventos/Estados e Eventos - Carrousel/src/App.tsx
+++ M2 - Front End/S2 - Introdução ao React  e TypeScript/2.5 - Estados e Eventos/Estados e Eventos - Carrousel/src/App.tsx
@@ -14,9 +14,11 @@ function App() {
   ];
 
   const [index, setIndex] = useState(0);
+  const [toolsList, setToolsList] = useState(toolKit);
+  const [inputValue, setInputValue] = useState('');
 
   function handleNextClick() {
-    if (index + 1 < toolKit.length) {
+    if (index + 1 < toolsList.length) {
       setIndex(index + 1);
     } else {
       setIndex(0); // Volta para o primeiro elemento
@@ -27,15 +29,37 @@ function App() {
     if (index - 1 >= 0) {
       setIndex(index - 1);
     } else {
-      setIndex(toolKit.length - 1); // Vai para o último elemento
+      setIndex(toolsList.length - 1); // Vai para o último elemento
+    }
+  }
+
+  function handleAddClick() {
+    if (inputValue) {
+      setToolsList([...toolsList, inputValue]);
     }
   }
   return (
     <>
       <h1>Caixa de ferramentas de uma Pessoa Desenvolvedora</h1>
-      <h2>{toolKit[index]}</h2>
+      <h2>{toolsList[index]}</h2>
+
       <button onClick={handlePreviousClick}>Anterior</button>
       <button onClick={handleNextClick}>Próximo</button>
+
+      <section>
+        <h3>Adicione novas ferramentas:</h3>
+        <input onChange={({ target }) => setInputValue(target.value) } />
+        <button onClick={handleAddClick}>Adicionar</button>
+      </section>
+
+      <section>
+        <h3>Lista de itens do carrossel:</h3>
+        <ul>
+          {toolsList.map((tool) => (
+            <li key={tool}>{tool}</li>
+          ))}
+        </ul>
+      </section>
     </>
   )
 }
