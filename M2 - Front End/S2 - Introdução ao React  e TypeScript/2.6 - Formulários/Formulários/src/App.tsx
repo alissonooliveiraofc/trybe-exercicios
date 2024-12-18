bd8eddb34eab51855591e4865399b480541acad9
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-12-18 19:59:17 -0300

    FRONT 2.6 - Finaliza validação dos campos

--- M2 - Front End/S2 - Introdução ao React  e TypeScript/2.6 - Formulários/Formulários/src/App.tsx
+++ M2 - Front End/S2 - Introdução ao React  e TypeScript/2.6 - Formulários/Formulários/src/App.tsx
@@ -6,13 +6,20 @@ function App() {
   const [email, setEmail] = useState(''); //estado do email
   const [schooling, setSchooling] = useState('Médio'); //estado da escolaridade
   const [resume, setResume] = useState(''); //estado do currículo
+  const [termos, setTermos] = useState(false); //estado dos termos
+  const [error, setError] = useState(false); // estado do erro
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    resetForm();
-    alert(
-    `Nome: ${name}\nemail: ${email}\nEscolaridade: ${schooling}\nExperiências: ${resume}`
-  )};
+    if (termos) {
+      alert(
+        `Nome: ${name}\nemail: ${email}\nEscolaridade: ${schooling}\nExperiências: ${resume}`
+      );
+      resetForm();
+    } else {
+      setError(true);
+    }
+  }
 
   function resetForm() {
     setName('');
@@ -47,12 +54,27 @@ function App() {
           Resumo das experiências
           <textarea
             maxLength={500} 
-            style={{ width: '300px', height: '150px', backgroundColor: 'black', padding: '10px' }}
+            style={{ width: '421px', height: '150px', backgroundColor: 'black', padding: '10px' }}
             value={resume}
             onChange={({ target }) => setResume(target.value)}
           />
         </label>
+
+        <label className='terms'>
+          Aceito os termos e condições
+        <input
+          type="checkbox"
+          checked={termos}
+          onChange={() => setTermos((prevTerms) => !prevTerms)}
+          />
+        </label>
         <button>Enviar</button>
+        {error && (
+        <h4 style={{color: 'red'}}>
+          Você precisa aceitar os termos e condições para poder enviar o
+          currículo!
+        </h4>
+      )}
       </form>
     </>
   )
