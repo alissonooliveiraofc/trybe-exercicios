e3865939d284c43cf3ea46927c7402d83cbb3854
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-12-18 17:52:15 -0300

    FRONT 2.6 - Finaliza aplicação principal

--- M2 - Front End/S2 - Introdução ao React  e TypeScript/2.6 - Formulários/Formulários/src/App.tsx
+++ M2 - Front End/S2 - Introdução ao React  e TypeScript/2.6 - Formulários/Formulários/src/App.tsx
@@ -1,21 +1,43 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [name, setName] = useState(''); //estado do nome
+  const [email, setEmail] = useState(''); //estado do email
+  const [schooling, setSchooling] = useState('Médio'); //estado da escolaridade
+  const [resume, setResume] = useState(''); //estado do currículo
 
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    resetForm();
+    alert(
+    `Nome: ${name}\nemail: ${email}\nEscolaridade: ${schooling}\nExperiências: ${resume}`
+  )};
+
+  function resetForm() {
+    setName('');
+    setEmail('');
+    setSchooling('Médio');
+    setResume('');
+  }
+
+  console.log(name, email, schooling, resume);
+  
+  
   return (
     <>
-      <form>
+      <form onSubmit={(event) => handleSubmit(event)}>
         <label>
           Nome
-          <input />
+          <input value={name} onChange={(event) => setName(event.target.value)} />
         </label>
         <label>
           E-mail
-          <input />
+          <input value={email} onChange={(event) => setEmail(event.target.value)} />
         </label>
         <label>
           Escolaridade
-          <select>
+          <select value={schooling} onChange={({ target }) => setSchooling(target.value)}>
             <option value="Médio">Médio</option>
             <option value="Superior">Superior</option>
             <option value="Pós-graduação">Pós-graduação</option>
@@ -23,7 +45,12 @@ function App() {
         </label>
         <label>
           Resumo das experiências
-          <textarea maxLength={500} style={{ width: '300px', height: '150px', backgroundColor: 'black', padding: '10px' }} />
+          <textarea
+            maxLength={500} 
+            style={{ width: '300px', height: '150px', backgroundColor: 'black', padding: '10px' }}
+            value={resume}
+            onChange={({ target }) => setResume(target.value)}
+          />
         </label>
         <button>Enviar</button>
       </form>
