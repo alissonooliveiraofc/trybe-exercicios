8d0c488b890351421cc7cbddb48c3674ac2af052
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-01-19 22:15:09 -0300

    FRONT 3.1 - Cria rotas para 3 páginas, coffe, coffe-list e home

--- M2 - Front End/S3 - Aprofundando no React/3.1 - React Router/exemplo de rotas/src/App.tsx
+++ M2 - Front End/S3 - Aprofundando no React/3.1 - React Router/exemplo de rotas/src/App.tsx
@@ -1,13 +1,18 @@
-
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Coffe from './pages/coffee';
+import CoffeList from './pages/coffee-list';
+import './App.css';
 
 function App() {
 
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/coffee" element={<Coffe />} />
+      <Route path="/coffee-list" element={<CoffeList />} />
+    </Routes>
   )
 }
 
-export default App
+export default App;
