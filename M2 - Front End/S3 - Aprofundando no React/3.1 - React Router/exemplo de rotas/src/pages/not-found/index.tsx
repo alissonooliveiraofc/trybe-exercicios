fb35948199d40f18c6d44ff2cd53632cdd63d719
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-01-23 23:08:46 -0300

    FRONT 3.1 - Estliza rota inexistente

--- M2 - Front End/S3 - Aprofundando no React/3.1 - React Router/exemplo de rotas/src/pages/not-found/index.tsx
+++ M2 - Front End/S3 - Aprofundando no React/3.1 - React Router/exemplo de rotas/src/pages/not-found/index.tsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
+import './not-found.css';
 
 function NotFound() {
   return (
-    <>
+    <div>
       <h1>Oops! Página não encontrada!</h1>
-      <h2 style={ { marginTop: '25px'}}>Desculpe, a página que você está procurando não existe =/</h2>
+
+      <img style={ {width: '350px'}} src="https://eloarmarinho.com.br/img/icons/404.png" alt="404 Error Message" />
+
+      <h2 style={ { marginTop: '50px'}}>Desculpe, a página que você está procurando não existe =/</h2>
       <Link to="/">
         <h3 style={{ marginTop: '30px' }}>Voltar à página inicial</h3>
       </Link>
-    </>
+    </div>
   )
 }
 
