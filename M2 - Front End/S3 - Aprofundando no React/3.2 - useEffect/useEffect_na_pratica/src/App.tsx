f47ff7e7c8210a874656d04922d0cb80a8c5cdad
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-01-30 21:11:15 -0300

    FRONT 3.2 - Renderiza as infos da API

--- M2 - Front End/S3 - Aprofundando no React/3.2 - useEffect/useEffect_na_pratica/src/App.tsx
+++ M2 - Front End/S3 - Aprofundando no React/3.2 - useEffect/useEffect_na_pratica/src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { fetchCoordinates } from './services';
 import './App.css';
 
 type Coordinates = {
@@ -8,10 +9,26 @@ type Coordinates = {
 
 function App() {
   const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCoordinates();
+      setCoordinates({ latitude: data.latitude, longitude: data.longitude });
+    }
+    fetchData();
+  }, []);
+
   return (
+    <>
     <h1>International Space Station Location Tracker</h1>
-  );
-}
+    {coordinates && (
+      <>
+        <h2>{`Latitude: ${coordinates.latitude}`}</h2>
+        <h2>{`Longitude: ${coordinates.longitude}`}</h2>
+      </>
+    )}
+    </>
+)};
 
 export default App;
 
