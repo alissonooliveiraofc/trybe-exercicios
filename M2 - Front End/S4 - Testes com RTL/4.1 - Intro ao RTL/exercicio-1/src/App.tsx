13a755c64a05ef39a02b7aea6e7fdf50336fd883
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-03-26 16:11:26 -0300

    FRONT 4.1 - Adiciona componente ValidEmail e implementa lógica para validação de email no formulário

--- M2 - Front End/S4 - Testes com RTL/4.1 - Intro ao RTL/exercicio-1/src/App.tsx
+++ M2 - Front End/S4 - Testes com RTL/4.1 - Intro ao RTL/exercicio-1/src/App.tsx
@@ -1,17 +1,34 @@
-// src/App.tsx
-import './App.css';
+import { useState } from 'react';
+import ValidEmail from './components/ValidEmail';
 
 function App() {
+  const [email, setEmail] = useState('')
+  const [title, setTitle] = useState('')
+
+  function handleClick() {
+    setTitle(email);
+    setEmail('');
+  }
   return (
     <div className="App">
       <label htmlFor="id-email">
         Email:
-        <input id="id-email" type="email" />
+        <input
+          id="id-email"
+          type="email" 
+          onChange={ ({target}) => setEmail(target.value)}
+          value={ email }
+        />
       </label>
-
-      <input data-testid="id-send" type="button" value="Enviar" />
-      
-      <input type="button" value="Voltar" />
+      <input
+        id="btn-send"
+        type="button"
+        data-testid="id-send"
+        value="Enviar"
+        onClick={ handleClick }
+        />
+      <input id="btn-back" type="button" value="Voltar" />
+      <ValidEmail email={ title } />
     </div>
   );
 }
