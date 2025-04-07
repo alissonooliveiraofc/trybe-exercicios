8c4ac190e449963d908f55fe5329b7f51d996716
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-04-07 18:07:27 -0300

    FRONT 4.2 - Atualiza configuração do TypeScript para incluir tipos personalizados e desativa verificação estrita

---
+++ M2 - Front End/S4 - Testes com RTL/4.2 - Mocks/testando apis e inputs/src/types.d.ts
@@ -0,0 +1,7 @@
+// filepath: src/types.d.ts
+declare module '@babel/core';
+declare module '@babel/generator';
+declare module '@babel/template';
+declare module '@babel/traverse';
+declare module 'estree';
+declare module 'json-schema';
