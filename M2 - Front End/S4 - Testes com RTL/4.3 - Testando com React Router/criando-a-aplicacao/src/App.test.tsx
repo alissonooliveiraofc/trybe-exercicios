4a4c0b62f5a48e1011cde06988b8b7730b2cac18
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-06-16 17:52:46 -0300

    FRONT 4.3 - Adiciona testes para navegação entre páginas no componente App

--- M2 - Front End/S4 - Testes com RTL/4.3 - Testando com React Router/criando-a-aplicacao/src/App.test.tsx
+++ M2 - Front End/S4 - Testes com RTL/4.3 - Testando com React Router/criando-a-aplicacao/src/App.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
+// usando a renderização padrão
 it('Renderiza texto da página inicial', () => {
   render(
     <BrowserRouter>
@@ -10,4 +11,14 @@ it('Renderiza texto da página inicial', () => {
     </BrowserRouter>
   );
   expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
+});
+
+// usando a renderização com wrapper
+it('Navega para página About', async () => {
+  render(<App />, { wrapper: BrowserRouter });
+  expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
+
+  const aboutLink = screen.getByRole('link', { name: /Sobre/i });
+  await userEvent.click(aboutLink);
+  expect(screen.getByText(/Você está na página Sobre/i)).toBeInTheDocument();
 })
