b4452e1bdfba12c789b4e68c9092a60a94fd563a
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-06-16 17:10:21 -0300

    FRONT 4.3 - adiciona rtl a aplicação

---
+++ M2 - Front End/S4 - Testes com RTL/4.3 - Testando com React Router/criando-a-aplicacao/src/App.test.tsx
@@ -0,0 +1,10 @@
+// src/App.test.tsx
+
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+it('Renderiza texto da página inicial', () => {
+  render(<App />);
+  expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
+})
